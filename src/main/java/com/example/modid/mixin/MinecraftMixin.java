37e0f31774090863f8fe857621a7e70c9869abe7
arcade_kappa <gkappa@outlands.top>
2025-04-02 20:58:36 +0800

    Add basic Mixin setup

---
+++ src/main/java/com/example/modid/mixin/MinecraftMixin.java
@@ -0,0 +1,16 @@
+package com.example.modid.mixin;
+
+import com.example.modid.ExampleMod;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MinecraftMixin {
+    @Inject(method = "createDisplay", at = @At("HEAD"))
+    public void inject(CallbackInfo ci){
+        ExampleMod.LOGGER.info("Mixin succeed!");
+    }
+}
