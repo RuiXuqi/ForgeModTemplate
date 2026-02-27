16f190d61955a9d6fc5ba93240c6e908650233e7
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2026-02-27 16:28:37 +0800

    Sync main

--- src/main/java/com/example/modid/ExampleMod.java
+++ src/main/java/com/example/modid/ExampleMod.java
@@ -1,6 +1,8 @@
 package com.example.modid;
 
+import com.example.modid.proxy.IProxy;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,6 +12,8 @@ public class ExampleMod {
 
     public static final Logger LOGGER = LogManager.getLogger(Reference.MOD_NAME);
 
+    @SidedProxy(modId = Reference.MOD_ID, clientSide = "com.example.modid.proxy.ClientProxy", serverSide = "com.example.modid.proxy.CommonProxy")
+    public static IProxy proxy;
     /**
      * <a href="https://cleanroommc.com/wiki/forge-mod-development/event#overview">
      *     Take a look at how many FMLStateEvents you can listen to via the @Mod.EventHandler annotation here
@@ -18,6 +22,7 @@ public class ExampleMod {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         LOGGER.info("Hello From {}!", Reference.MOD_NAME);
+        LOGGER.info("Proxy is {}", proxy);
     }
 
 }
